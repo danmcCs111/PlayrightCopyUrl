d3d109b1ecd8fa15ab794715f671b409c985a73d
danmcCs111 <danmccs111@gmail.com>
2025-07-29 00:45:29 -0400

    update to use comma seperation

--- PlayrightVideo/GrabScripts/parseWeather.sh
+++ PlayrightVideo/GrabScripts/parseWeather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 fle="../GrabFolder/Weather/weather_grab*"
-egrep -o "<b>.*Temperature: [0-9]+ .*Thunder: .* onclick\=" $fle | sed 's/&amp;//g' | sed 's/nbsp;/ /g' | sed 's/<[/]*b[r]*>/ /g' | sed "s/')\" onclick=//g"
+egrep -o "<b>.*Temperature: [0-9]+ .*Thunder: .* onclick\=" $fle | sed 's/,/ -/g' | sed 's/&amp;//g' | sed 's/nbsp;/,/g' | sed 's/<[/]*b[r]*>/ /g' | sed "s/')\" onclick=//g"
