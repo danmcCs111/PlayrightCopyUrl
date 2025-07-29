1dc940493b9aa09bd95893b820a6e112e59725a2
danmcCs111 <danmccs111@gmail.com>
2025-07-29 01:22:22 -0400

    add output

--- PlayrightVideo/GrabScripts/parseWeather.sh
+++ PlayrightVideo/GrabScripts/parseWeather.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-fle="../GrabFolder/Weather/weather_grab*"
-egrep -o "<b>.*Temperature: [0-9]+ .*Thunder: .* onclick\=" $fle | sed 's/,/ -/g' | sed 's/&amp;//g' | sed 's/nbsp;/,/g' | sed -e "s/[,]\{5,\}/\",\"/g" | sed 's/<[/]*b[r]*>/ /g' | sed "s/')\" onclick=//g" | sed -e 's/^/\"/g' | sed -e 's/$/\"/g'
+fle="../GrabFolder/Weather/weather_grab*.html"
+fle_save=`ls ../GrabFolder/Weather/weather_grab*`.csv
+egrep -o "<b>.*Temperature: [0-9]+ .*Thunder: .* onclick\=" $fle | sed 's/,/ -/g' | sed 's/&amp;/,/g' | sed 's/nbsp;/,/g' | sed -e "s/[,]\{5,\}/\",\"/g" | sed 's/<[/]*b[r]*>/ /g' | sed "s/')\" onclick=//g" | sed -e 's/^/\"/g' | sed -e 's/$/\"/g' > $fle_save
