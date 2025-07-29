c1689c3d2ac469ff8c3b4b033521b4d3166a0cb5
danmcCs111 <danmccs111@gmail.com>
2025-07-29 00:39:00 -0400

    correct parsing on thunder

--- PlayrightVideo/GrabScripts/parseWeather.sh
+++ PlayrightVideo/GrabScripts/parseWeather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 fle="../GrabFolder/Weather/weather_grab*"
-egrep -o "<b>.*Temperature: [0-9]+ .*Thunder: [A-Za-z ]* [-\)\(0-9\% ]*" $fle | sed 's/&amp;//g' | sed 's/nbsp;/ /g' | sed 's/<[/]*b[r]*>/ /g'
+egrep -o "<b>.*Temperature: [0-9]+ .*Thunder: .* onclick\=" $fle | sed 's/&amp;//g' | sed 's/nbsp;/ /g' | sed 's/<[/]*b[r]*>/ /g' | sed "s/')\" onclick=//g"
