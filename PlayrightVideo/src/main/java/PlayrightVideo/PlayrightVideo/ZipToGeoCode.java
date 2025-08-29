3ee0fad087f2bae0f6e464aa2f4edd3b3e129635
danmcCs111 <danmccs111@gmail.com>
2025-08-29 11:52:57 -0400

    update method

--- PlayrightVideo/src/main/java/PlayrightVideo/PlayrightVideo/ZipToGeoCode.java
+++ PlayrightVideo/src/main/java/PlayrightVideo/PlayrightVideo/ZipToGeoCode.java
@@ -18,43 +18,43 @@ public class ZipToGeoCode
     public static void main(String[] args)
     {
     	String zipStr = args[args.length-1];
-    	
+    	String [] latLong = collectCoordinates(zipStr);
+    	System.out.println(latLong[0] + " " + latLong[1]);
+    }
+    
+    
+    public static String [] collectCoordinates(String zipStr)
+    {
+    	String [] latLongRet = new String[2];
     	try (Playwright playwright = Playwright.create()) 
     	{
     		LaunchOptions lo = new LaunchOptions();
     		Browser browser = playwright.firefox().launch(lo);
     		Page page = App.navigateAndReturnPage(browser, url);
-    		String [] latLong = collectCoordinates(page, zipStr);
-    		System.out.println(latLong[0] + " " + latLong[1]);
+    		Locator l = page.locator(textBox);
+    		l.fill(zipStr);
+    		
+    		System.out.println(l);
+    		System.out.println(l.inputValue());
+    		Locator l2 = page.locator(btnLookup);
+    		System.out.println(l2);
+    		for(String s : l2.allTextContents())
+    			System.out.println(s);
+    		l2.click();
+    		page.waitForURL(url);
+    		page.waitForLoadState();
+    		
+    		Locator l4 = page.locator(zip);
+    		l4.waitFor();
+    		
+    		Locator l3 = page.locator(latLong);
+    		String retStr = "";
+    		for(String s : l3.allTextContents())
+    			retStr = s;
+    		latLongRet = retStr.split(",");
     	}
-    }
-    
-    
-    public static String [] collectCoordinates(Page page, String zipStr)
-    {
-    	Locator l = page.locator(textBox);
-		l.fill(zipStr);
-		
-		System.out.println(l);
-		System.out.println(l.inputValue());
-		Locator l2 = page.locator(btnLookup);
-		System.out.println(l2);
-		for(String s : l2.allTextContents())
-			System.out.println(s);
-		l2.click();
-		page.waitForURL(url);
-		page.waitForLoadState();
-		
-		Locator l4 = page.locator(zip);
-		l4.waitFor();
-		
-		Locator l3 = page.locator(latLong);
-		String retStr = "";
-		for(String s : l3.allTextContents())
-			retStr = s;
-		String [] ss = retStr.split(",");
-		
-		return ss;
+    	
+		return latLongRet;
     }
     
   }
