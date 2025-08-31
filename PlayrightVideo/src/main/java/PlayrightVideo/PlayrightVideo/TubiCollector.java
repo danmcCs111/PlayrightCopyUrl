b933826b58baa5e5d8e4462be88093dedeaf96b7
danmcCs111 <danmccs111@gmail.com>
2025-08-31 14:17:03 -0400

    add save file of content collected

--- PlayrightVideo/src/main/java/PlayrightVideo/PlayrightVideo/TubiCollector.java
+++ PlayrightVideo/src/main/java/PlayrightVideo/PlayrightVideo/TubiCollector.java
@@ -10,12 +10,16 @@ public class TubiCollector
 	
 	public static void main(String [] args)
 	{
+		String sourceFile = args[args.length-1];
+		
 		try (Playwright playwright = Playwright.create()) 
 		{
 			Browser browser = playwright.chromium().launch();
 			Page page = PlayrightUtil.tubiNavigateAndReturnPage(browser, url);
 			String contentString = page.content();
 			page.close();
+			
+			sourceFile = PlayrightUtil.writeFile(sourceFile, contentString);
 			
 			System.out.println(contentString);
 		}
