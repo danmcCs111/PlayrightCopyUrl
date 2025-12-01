4f0b4039fd13cf6ae584ff595ca6eca093db20f6
danmcCs111 <danmccs111@gmail.com>
2025-12-01 12:11:38 -0500

    add environment variable for classpath jars

--- PlayrightVideo/GrabScripts/Tubi/collectAllTubiUrls.sh
+++ PlayrightVideo/GrabScripts/Tubi/collectAllTubiUrls.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+source ./java-class-path.sh
+
 fileName="../../GrabFolder/Tubi/tubiHomePage.txt"
 fileName2="../../GrabFolder/Tubi/tubiCollector.txt"
 
-java -cp "../../target/dependency/driver-1.53.0.jar;../../target/dependency/driver-bundle-1.53.0.jar;../../target/dependency/error_prone_annotations-2.36.0.jar;../../target/dependency/gson-2.12.1.jar;../../target/dependency/junit-3.8.1.jar;../../target/dependency/opentest4j-1.3.0.jar;../../target/dependency/playwright-1.53.0.jar;../../target/playright-video-0.0.1-SNAPSHOT.jar" PlayrightVideo.PlayrightVideo.TubiHomePage npx playwright codegen demo.playwright.dev/todomvc "$fileName"
+java -cp "$java_playright_cp" PlayrightVideo.PlayrightVideo.TubiHomePage npx playwright codegen demo.playwright.dev/todomvc "$fileName"
 
 urls=`./tubiUrlStrip.sh "$fileName"`
 for a in ${urls[@]};
 do
  echo $a
- java -cp "../../target/dependency/driver-1.53.0.jar;../../target/dependency/driver-bundle-1.53.0.jar;../../target/dependency/error_prone_annotations-2.36.0.jar;../../target/dependency/gson-2.12.1.jar;../../target/dependency/junit-3.8.1.jar;../../target/dependency/opentest4j-1.3.0.jar;../../target/dependency/playwright-1.53.0.jar;../../target/playright-video-0.0.1-SNAPSHOT.jar" PlayrightVideo.PlayrightVideo.TubiCollector npx playwright codegen demo.playwright.dev/todomvc $a $fileName2; ./tubiCollector.sh $fileName2
+ java -cp "$java_playright_cp" PlayrightVideo.PlayrightVideo.TubiCollector npx playwright codegen demo.playwright.dev/todomvc $a $fileName2; ./tubiCollector.sh $fileName2
 done
--- PlayrightVideo/GrabScripts/Tubi/tubiDownloadPosterArt.sh
+++ PlayrightVideo/GrabScripts/Tubi/tubiDownloadPosterArt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source ./java-class-path.sh
 fileName=$1
 fileNameTmp=$2
 mov=$3
@@ -18,9 +18,9 @@ fileNameTmp=$fileNameTmp$movTmp
 
 echo $url $movSave $movTmp $mov
 
-java -cp "../../target/dependency/driver-1.53.0.jar;../../target/dependency/driver-bundle-1.53.0.jar;../../target/dependency/error_prone_annotations-2.36.0.jar;../../target/dependency/gson-2.12.1.jar;../../target/dependency/junit-3.8.1.jar;../../target/dependency/opentest4j-1.3.0.jar;../../target/dependency/playwright-1.53.0.jar;../../target/playright-video-0.0.1-SNAPSHOT.jar" PlayrightVideo.PlayrightVideo.TubiSingleCollect npx playwright codegen demo.playwright.dev/todomvc $url $fileNameTmp
+java -cp "$java_playright_cp" PlayrightVideo.PlayrightVideo.TubiSingleCollect npx playwright codegen demo.playwright.dev/todomvc $url $fileNameTmp
 poster=`egrep -o "\"posterarts\"\:\[\"https:[^\".]*canvas-lb.tubitv.com[^\".]*" $fileNameTmp | sed 's/\u002F//g' | grep -o https.*`
 
-java -cp "../../target/dependency/driver-1.53.0.jar;../../target/dependency/driver-bundle-1.53.0.jar;../../target/dependency/error_prone_annotations-2.36.0.jar;../../target/dependency/gson-2.12.1.jar;../../target/dependency/junit-3.8.1.jar;../../target/dependency/opentest4j-1.3.0.jar;../../target/dependency/playwright-1.53.0.jar;../../target/playright-video-0.0.1-SNAPSHOT.jar" PlayrightVideo.PlayrightVideo.TubiDownloadPosterArt npx playwright codegen demo.playwright.dev/todomvc $poster "$fileName" $movSave
+java -cp "$java_playright_cp" PlayrightVideo.PlayrightVideo.TubiDownloadPosterArt npx playwright codegen demo.playwright.dev/todomvc $poster "$fileName" $movSave
 
 rm $fileNameTmp
