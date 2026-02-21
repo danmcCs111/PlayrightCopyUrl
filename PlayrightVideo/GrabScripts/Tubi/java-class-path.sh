7b95c5687d388590237a27bda320561a80b16f5c
danmcCs111@gmail.com <danmcCs111@gmail.com>
2026-02-20 19:35:05 -0500

    correct typo

--- PlayrightVideo/GrabScripts/Tubi/java-class-path.sh
+++ PlayrightVideo/GrabScripts/Tubi/java-class-path.sh
@@ -3,7 +3,7 @@
 typeOs=`uname`
 if [[ "$typeOs" == "Linux" ]]
 then
-	export java_playright_cp="../../target/dependency/driver-1.53.0.jar:../../target/dependency/driver-bundle-1.53.0.jar:../../target/dependency/error_prone_annotations-2.36.0.jar:../../target/dependency/gson-2.12.1.jar:../../target/dependency/junit-3.8.1.jar:../../target/dependency/opentest4j-1.3.0.jar;../../target/dependency/playwright-1.53.0.jar:../../target/playright-video-0.0.1-SNAPSHOT.jar"
+	export java_playright_cp="../../target/dependency/driver-1.53.0.jar:../../target/dependency/driver-bundle-1.53.0.jar:../../target/dependency/error_prone_annotations-2.36.0.jar:../../target/dependency/gson-2.12.1.jar:../../target/dependency/junit-3.8.1.jar:../../target/dependency/opentest4j-1.3.0.jar:../../target/dependency/playwright-1.53.0.jar:../../target/playright-video-0.0.1-SNAPSHOT.jar"
 else
 	export java_playright_cp="../../target/dependency/driver-1.53.0.jar;../../target/dependency/driver-bundle-1.53.0.jar;../../target/dependency/error_prone_annotations-2.36.0.jar;../../target/dependency/gson-2.12.1.jar;../../target/dependency/junit-3.8.1.jar;../../target/dependency/opentest4j-1.3.0.jar;../../target/dependency/playwright-1.53.0.jar;../../target/playright-video-0.0.1-SNAPSHOT.jar"
 fi
