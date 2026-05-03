697cc9f4563cf9420ee51d54d1073005f7b82d28
danmcCs111 <danmccs111@gmail.com>
2026-05-03 19:44:31 -0400

    change to refrence reprocess, always use diff since saving images

--- PlayrightVideo/GrabScripts/Tubi/tubiDownloadCollectionPosterArt.sh
+++ PlayrightVideo/GrabScripts/Tubi/tubiDownloadCollectionPosterArt.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
-
-typeOs=`uname`
-if [[ "$typeOs" == "Linux" ]]
-then
-	fileName="../../GrabFolder/Tubi/images/"
-	fileNameTmp="../../GrabFolder/Tubi/tmp/"
-else
-	fileName="..\\..\\GrabFolder\\Tubi\\images\\"
-	fileNameTmp="..\\..\\GrabFolder\\Tubi\\tmp\\"
-fi
-
-movies=`ls ../../GrabFolder/Tubi/*.url`
-
-count=1
-paralellCount=6
-for mov in ${movies[@]}; 
-do
-	if [ $(expr $count % $paralellCount) -eq 0 ]
-	then
-		sleep 10
-	else
-	./tubiDownloadPosterArt.sh $fileName $fileNameTmp $mov&
-	fi
-
-	count=$(( $count + 1))
-done
+./tubiReprocessPosterArt.sh
