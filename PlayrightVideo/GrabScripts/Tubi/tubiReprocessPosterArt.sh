63ad736fd6e5b9fb35bcc0275c5834ffa710d63e
danmcCs111 <danmccs111@gmail.com>
2026-06-30 21:38:37 -0400

    update wait time, add progress

--- PlayrightVideo/GrabScripts/Tubi/tubiReprocessPosterArt.sh
+++ PlayrightVideo/GrabScripts/Tubi/tubiReprocessPosterArt.sh
@@ -12,20 +12,21 @@ else
 	fileNameTmp="..\\..\\GrabFolder\\Tubi\\tmp\\"
 fi
 
-movies=`diff  <(ls -1 ../../GrabFolder/Tubi/*.url | sed 's/..\/..\/GrabFolder\/Tubi\///g' | sed s/.url//g) <( ls -1 ../../GrabFolder/Tubi/images/*.png | sed 's/..\/..\/GrabFolder\/Tubi\/images\///g' | sed s/.png//g) | egrep "^<" | egrep "^<|^>" | sed "s/[<>]//g"`
-
+movies=(`diff  <(ls -1 ../../GrabFolder/Tubi/*.url | sed 's/..\/..\/GrabFolder\/Tubi\///g' | sed s/.url//g) <( ls -1 ../../GrabFolder/Tubi/images/*.png | sed 's/..\/..\/GrabFolder\/Tubi\/images\///g' | sed s/.png//g) | egrep "^<" | egrep "^<|^>" | sed "s/[<>]//g"`)
+total_count=${#movies[@]}
 
 count=1
-paralellCount=3
+parallelCount=3
 for mov in ${movies[@]}; 
 do
 	mov=$mov".url"
 	echo $mov
-	if [ $(expr $count % $paralellCount) -eq 0 ]
+	if [ $(expr $count % $parallelCount) -eq 0 ]
 	then
-		sleep 3
+		echo "remaining: " $(( $count )) "/" $total_count
+		sleep 6
 	else
-	./tubiDownloadPosterArt.sh $fileName $fileNameTmp $mov&
+		./tubiDownloadPosterArt.sh $fileName $fileNameTmp $mov&
 	fi
 
 	count=$(( $count + 1))
