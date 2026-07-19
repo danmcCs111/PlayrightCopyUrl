45ee91997216be3ca5f986593bc8695362b40ebf
danmcCs111 <danmccs111@gmail.com>
2026-07-19 08:32:17 -0400

    revert back to background process

--- PlayrightVideo/GrabScripts/Tubi/tubiReprocessPosterArt.sh
+++ PlayrightVideo/GrabScripts/Tubi/tubiReprocessPosterArt.sh
@@ -16,19 +16,18 @@ movies=(`diff  <(ls -1 ../../GrabFolder/Tubi/*.url | sed 's/..\/..\/GrabFolder\/
 total_count=${#movies[@]}
 
 count=1
-#parallelCount=3
+parallelCount=3
 for mov in ${movies[@]}; 
 do
 	mov=$mov".url"
 	echo $mov
-#if [ $(expr $count % $parallelCount) -eq 0 ]
-#then
-	echo "remaining: " $(( $count )) "/" $total_count
-#sleep 9
-#else
-	./tubiDownloadPosterArt.sh $fileName $fileNameTmp $mov
-#fi
-
+	if [ $(expr $count % $parallelCount) -eq 0 ]
+	then
+		echo "remaining: " $(( $count )) "/" $total_count
+		sleep 9
+	else
+		./tubiDownloadPosterArt.sh $fileName $fileNameTmp $mov&
+	fi
 	count=$(( $count + 1))
 done
 cd $crDir
